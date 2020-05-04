5fe1da0d910f7132f49add7f9c080697b9556a57
Jona <s_heitzer@stud.hwr-berlin.de>
2020-05-04 15:05:00 +0200

    Add redstar/kernel/.gitignore

--- redstar/kernel/arch/i386/tty.d
+++
@@ -1,6 +0,0 @@
-arch/i386/tty.o: arch/i386/tty.c \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stdbool.h \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stddef.h \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stdint.h \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stdint-gcc.h \
- arch/i386/../../include/kernel/tty.h arch/i386/vga.h
--- redstar/kernel/kernel/kernel.d
+++
@@ -1,6 +0,0 @@
-kernel/kernel.o: kernel/kernel.c \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stdbool.h \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stddef.h \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stdint.h \
- /home/jona/bin/cross/lib/gcc/i686-elf/9.3.0/include/stdint-gcc.h \
- kernel/../include/kernel/tty.h
