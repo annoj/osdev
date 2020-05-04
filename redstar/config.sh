3b75588e02262b47bfb6462f0d74b7a7e80d8c02
Jona <s_heitzer@stud.hwr-berlin.de>
2020-05-04 14:18:56 +0200

    Add redstar/config.sh

---
+++ redstar/config.sh
@@ -0,0 +1,29 @@
+SYSTEM_HEADER_PROJECTS="kernel"
+PROJECTS="kernel"
+
+CROSSDIR=/home/jona/bin/cross/bin
+
+export HOST=${HOST:-$(./host.sh)}
+export MAKE=/usr/bin/make
+
+export AR=${CROSSDIR}${HOST}-ar
+export AS=${CROSSDIR}${HOST}-as
+export CC=${CROSSDIR}${HOST}-gcc
+
+export PREFIX=/usr
+export EXEC_PREFIX=$PREFIX
+export BOOTDIR=/boot
+export LIBDIR=$EXEC_PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
+
+export CFLAGS='-O2 -g'
+
+# Configure the cross-compiler to use the desired system root.
+export SYSROOT="$(pwd)/sysroot"
+export CC="$CC --sysroot=$SYSROOT"
+
+# Work around that the -elf gcc targets doesn't have a system include directory
+# because it was configured with --without-headers rather than --with-sysroot.
+if echo "$HOST" | grep -Eq -- '-elf($|-)'; then
+	export CC="$CC -isystem=$INCLUDEDIR"
+fi
